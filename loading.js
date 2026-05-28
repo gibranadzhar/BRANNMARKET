d8c4e3b99919a7e91ee3d69be0aaab6b74af0889
gibranadzhar <gbrankahlil2@gmail.com>
2026-05-29 01:30:37 +0700

    Create loading.js

---
+++ loading.js
@@ -0,0 +1,25 @@
+window.addEventListener(
+  'load',
+  ()=>{
+
+    const loader =
+    document.getElementById(
+      'loader'
+    )
+
+    setTimeout(()=>{
+
+      loader.style.opacity =
+      '0'
+
+      setTimeout(()=>{
+
+        loader.style.display =
+        'none'
+
+      },500)
+
+    },1000)
+
+  }
+)
