fdfade6df14c340174695c3382b7a9612bc1cf82
gibranadzhar <gbrankahlil2@gmail.com>
2026-05-29 00:56:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,75 @@
+const toast = document.createElement('div')
+
+toast.className = 'toast'
+
+document.body.appendChild(toast)
+
+function showToast(message){
+
+  toast.innerText = message
+
+  toast.style.display = 'block'
+
+  setTimeout(()=>{
+
+    toast.style.display = 'none'
+
+  },3000)
+
+}
+
+window.showToast = showToast
+
+
+// DARK MODE SAVE
+
+const savedTheme =
+localStorage.getItem('theme')
+
+if(savedTheme === 'light'){
+
+  document.body.classList.add(
+    'light-mode'
+  )
+
+}
+
 const themeToggle =
 document.getElementById(
   'themeToggle'
 )
 
-themeToggle.addEventListener(
-  'click',
-  ()=>{
+if(themeToggle){
 
-    document.body.classList.toggle(
-      'light-mode'
-    )
+  themeToggle.addEventListener(
+    'click',
+    ()=>{
 
-  }
-)
+      document.body.classList.toggle(
+        'light-mode'
+      )
+
+      if(
+        document.body.classList.contains(
+          'light-mode'
+        )
+      ){
+
+        localStorage.setItem(
+          'theme',
+          'light'
+        )
+
+      }else{
+
+        localStorage.setItem(
+          'theme',
+          'dark'
+        )
+
+      }
+
+    }
+  )
+
+}
